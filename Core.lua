109e28bff570dca991875934992dd9a6f3034d5c
Archarodim <archarodim@teaser.fr>
2009-12-11 01:09:23 +0100

    - Internal cleanup (:retab, typos, etc...)

--- Core.lua
+++ Core.lua
@@ -8,7 +8,7 @@ Version @project-version@
 This is a very simple and light add-on that rings when you hover or target a
 unit of the opposite faction who healed someone during the last 60 seconds (can
 be configured).
-Now you can spot healers instantly and help them to die.
+Now you can spot those nasty healers instantly and help them to accomplish their destiny!
 
 This add-on uses the Ace3 framework.
 
@@ -31,30 +31,32 @@ local HHTD_C = {};
 hhtd.C = HHTD_C;
 
 HHTD_C.HealingClasses = {
-    ["PRIEST"]	= true,
-    ["PALADIN"]	= true,
-    ["DRUID"]	= true,
-    ["SHAMAN"]	= true,
+    ["PRIEST"]  = true,
+    ["PALADIN"] = true,
+    ["DRUID"]   = true,
+    ["SHAMAN"]  = true,
 };
 
 hhtd.EnemyHealers = {};
 
 
 -- upvalues
-local UnitIsPlayer	= _G.UnitIsPlayer;
-local UnitIsDead	= _G.UnitIsDead;
-local UnitFactionGroup	= _G.UnitFactionGroup;
-local UnitGUID		= _G.UnitGUID;
-local UnitIsUnit	= _G.UnitIsUnit;
-local UnitSex		= _G.UnitSex;
-local UnitClass		= _G.UnitClass;
-local UnitName		= _G.UnitName;
-local UnitFactionGroup	= _G.UnitFactionGroup;
-local GetTime		= _G.GetTime;
-local PlaySoundFile	= _G.PlaySoundFile;
+local UnitIsPlayer      = _G.UnitIsPlayer;
+local UnitIsDead        = _G.UnitIsDead;
+local UnitFactionGroup  = _G.UnitFactionGroup;
+local UnitGUID          = _G.UnitGUID;
+local UnitIsUnit        = _G.UnitIsUnit;
+local UnitSex           = _G.UnitSex;
+local UnitClass         = _G.UnitClass;
+local UnitName          = _G.UnitName;
+local UnitFactionGroup  = _G.UnitFactionGroup;
+local GetTime           = _G.GetTime;
+local PlaySoundFile     = _G.PlaySoundFile;
 
 
 -- }}} ]=]
+
+-- 03 Ghosts I
 
 -- [=[ options and defaults {{{
 local options = {
@@ -62,69 +64,69 @@ local options = {
     handler = hhtd,
     type = 'group',
     args = {
-	VersionHeader = {
-	    type = 'header',
-	    name = 'version: @project-version@',
-	    order = 1,
-	},
-	ReleaseDateHeader = {
-	    type = 'header',
-	    name = 'Release Date: @project-date-iso@',
-	    order = 2,
-	},
+        VersionHeader = {
+            type = 'header',
+            name = 'version: @project-version@',
+            order = 1,
+        },
+        ReleaseDateHeader = {
+            type = 'header',
+            name = 'Release Date: @project-date-iso@',
+            order = 2,
+        },
         on = {
             type = 'toggle',
             name = 'on',
             desc = 'Enables HHTD',
-	    set = function(info) hhtd.db.global.Enabled = hhtd:Enable(); return hhtd.db.global.Enabled; end,
+            set = function(info) hhtd.db.global.Enabled = hhtd:Enable(); return hhtd.db.global.Enabled; end,
             get = function(info) return hhtd:IsEnabled(); end,
-	    order = 10,
+            order = 10,
         },
-	off = {
+        off = {
             type = 'toggle',
             name = 'off',
             desc = 'Disables HHTD',
-	    set = function(info) hhtd.db.global.Enabled = not hhtd:Disable(); return not hhtd.db.global.Enabled; end,
+            set = function(info) hhtd.db.global.Enabled = not hhtd:Disable(); return not hhtd.db.global.Enabled; end,
             get = function(info) return not hhtd:IsEnabled(); end,
-	    order = 20,
+            order = 20,
         },
-	Header1 = {
-	    type = 'header',
-	    name = '',
-	    order = 25,
-	},
-	HFT = {
-	    type = "range",
-	    name = "Healer Forget Timer",
-	    desc = "Set the Healer Forget Timer (the time in seconds an enemy will remain considered has a healer)",
-	    min = 10,
-	    max = 60 * 10,
-	    step = 1,
-	    bigStep = 5,
-	    set = function(info, value) hhtd.db.global.HFT = value; return value; end,
-	    get = function(info) return hhtd.db.global.HFT; end,
-	    order = 30,
-	},
-	Header1000 = {
-	    type = 'header',
-	    name = '',
-	    order = 999,
-	},
-	debug = {
+        Header1 = {
+            type = 'header',
+            name = '',
+            order = 25,
+        },
+        HFT = {
+            type = "range",
+            name = "Healer Forget Timer",
+            desc = "Set the Healer Forget Timer (the time in seconds an enemy will remain considered has a healer)",
+            min = 10,
+            max = 60 * 10,
+            step = 1,
+            bigStep = 5,
+            set = function(info, value) hhtd.db.global.HFT = value; return value; end,
+            get = function(info) return hhtd.db.global.HFT; end,
+            order = 30,
+        },
+        Header1000 = {
+            type = 'header',
+            name = '',
+            order = 999,
+        },
+        debug = {
             type = 'toggle',
             name = 'debug',
             desc = 'Enables / disables debugging',
-	    set = function(info, value) hhtd.db.global.Debug = value; hhtd:Print("Debugging status is", value); return value; end,
+            set = function(info, value) hhtd.db.global.Debug = value; hhtd:Print("Debugging status is", value); return value; end,
             get = function(info) return hhtd.db.global.Debug end,
-	    order = 1000,
+            order = 1000,
         },
-	version = {
-	    type = 'execute',
-	    name = 'version',
-	    desc = 'Display version and release date',
-	    guiHidden = true,
-	    func = function () hhtd:Print('version: @project-version@, released on: @project-date-iso@') end,
-	    order = 1010,
+        version = {
+            type = 'execute',
+            name = 'version',
+            desc = 'Display version and release date',
+            guiHidden = true,
+            func = function () hhtd:Print('version: @project-version@, released on: @project-date-iso@') end,
+            order = 1010,
         },
     },
 }
@@ -181,72 +183,74 @@ function hhtd:TestUnit(EventName)
     local Unit="";
 
     if EventName=="UPDATE_MOUSEOVER_UNIT" then
-	Unit = "mouseover";
+        Unit = "mouseover";
     elseif EventName=="PLAYER_TARGET_CHANGED" then
-	Unit = "target";
+        Unit = "target";
     else
-	self:Print("called on unvalid event");
-	return;
+        self:Print("called on unvalid event");
+        return;
     end
 
     if not UnitIsPlayer(Unit) or UnitIsDead(Unit) then
-	return;
+        return;
     end
 
     local UnitFaction = UnitFactionGroup(Unit);
 
     if UnitFaction == PlayerFaction then
-	return;
+        return;
     end
 
     local TheUG = UnitGUID(Unit);
     local TheUnitClass_loc, TheUnitClass;
 
     if UnitIsUnit("mouseover", "target") then
-	--self:Debug("mouseover is target");
-	return;
+        --self:Debug("mouseover is target");
+        return;
     elseif LastDetectedGUID == TheUG and Unit == "target" then
-	--PlaySoundFile("Sound\\Doodad\\BellTollTribal.wav");
-	PlaySoundFile("Sound\\interface\\AuctionWindowOpen.wav");
-	local sex = UnitSex(Unit);
-	local what = (sex == 1 and "it" or sex == 2 and "him" or "her");
-	TheUnitClass_loc, TheUnitClass = UnitClass(Unit);
+        PlaySoundFile("Sound\\interface\\AuctionWindowOpen.wav");
+        self:Debug("AuctionWindowOpen.wav played");
 
-	self:Print("|cFF00FF22", ("You got %s!"):format(self:ColorText(what, self:GetClassHexColor(TheUnitClass))), "|r");
-	return;
-	
+        local sex = UnitSex(Unit);
+        local what = (sex == 1 and "it" or sex == 2 and "him" or "her");
+        TheUnitClass_loc, TheUnitClass = UnitClass(Unit);
+
+        self:Print("|cFF00FF22", ("You got %s!"):format(self:ColorText(what, self:GetClassHexColor(TheUnitClass))), "|r");
+        return;
+        
     end
 
     if not TheUG then
-	self:Debug("No unit GUID");
-	return;
+        self:Debug("No unit GUID");
+        return;
     end
 
     TheUnitClass_loc, TheUnitClass = UnitClass(Unit);
 
     if not TheUnitClass then
-	self:Debug("No unit Class");
-	return;
+        self:Debug("No unit Class");
+        return;
     end
 
     -- Is the unit class able to heal?
     if HHTD_C.HealingClasses[TheUnitClass] then
 
-	if hhtd.EnemyHealers[TheUG] then
-	    if (GetTime() - hhtd.EnemyHealers[TheUG]) > hhtd.db.global.HFT then
-		self:Debug((UnitName(Unit)), " did not heal for more than", hhtd.db.global.HFT, ", removed.");
-		hhtd.EnemyHealers[TheUG] = nil;
-	    else
-		if LastDetectedGUID ~= TheUG then
-		    self:Print("|cFFFF0000", ("%s is a healer!"):format(self:ColorText((UnitName(Unit)), self:GetClassHexColor(TheUnitClass))), "|r");
-		end
+        if hhtd.EnemyHealers[TheUG] then
+            if (GetTime() - hhtd.EnemyHealers[TheUG]) > hhtd.db.global.HFT then
+                self:Debug((UnitName(Unit)), " did not heal for more than", hhtd.db.global.HFT, ", removed.");
+                hhtd.EnemyHealers[TheUG] = nil;
+            else
+                if LastDetectedGUID ~= TheUG then
+                    self:Print("|cFFFF0000", ("%s is a healer!"):format(self:ColorText((UnitName(Unit)), self:GetClassHexColor(TheUnitClass))), "|r");
+                end
 
-		LastDetectedGUID = TheUG;
+                LastDetectedGUID = TheUG;
 
-		PlaySoundFile("Sound\\interface\\AlarmClockWarning3.wav");
-	    end
-	    
-	end
+                PlaySoundFile("Sound\\interface\\AlarmClockWarning3.wav");
+                self:Debug("AlarmClockWarning3.wav played");
+            end
+            
+        end
 
     end
 
@@ -265,10 +269,10 @@ function hhtd:CleanHealers()
     self:Debug("cleaning...");
 
     for GUID, LastHeal in pairs(hhtd.EnemyHealers) do
-	if (Time - LastHeal) > hhtd.db.global.HFT then
-	    hhtd.EnemyHealers[GUID] = nil;
-	    self:Debug(GUID, "removed");
-	end
+        if (Time - LastHeal) > hhtd.db.global.HFT then
+            hhtd.EnemyHealers[GUID] = nil;
+            self:Debug(GUID, "removed");
+        end
     end
 
     LastCleaned = Time;
@@ -276,32 +280,31 @@ function hhtd:CleanHealers()
 end
 
 do
-    local bit	    = _G.bit;
-    local band	    = _G.bit.band;
-    local bor	    = _G.bit.bor;
+    local bit       = _G.bit;
+    local band      = _G.bit.band;
+    local bor       = _G.bit.bor;
     local UnitGUID  = _G.UnitGUID;
-    local sub	    = _G.string.sub;
+    local sub       = _G.string.sub;
     local GetTime   = _G.GetTime;
     
-    local PET			= COMBATLOG_OBJECT_TYPE_PET;
+    local PET                   = COMBATLOG_OBJECT_TYPE_PET;
 
-    local OUTSIDER		= COMBATLOG_OBJECT_AFFILIATION_OUTSIDER;
-    local HOSTILE_OUTSIDER	= bit.bor (COMBATLOG_OBJECT_AFFILIATION_OUTSIDER, COMBATLOG_OBJECT_REACTION_HOSTILE);
-    local FRIENDLY_TARGET	= bit.bor (COMBATLOG_OBJECT_TARGET, COMBATLOG_OBJECT_REACTION_FRIENDLY);
+    local OUTSIDER              = COMBATLOG_OBJECT_AFFILIATION_OUTSIDER;
+    local HOSTILE_OUTSIDER      = bit.bor (COMBATLOG_OBJECT_AFFILIATION_OUTSIDER, COMBATLOG_OBJECT_REACTION_HOSTILE);
+    local FRIENDLY_TARGET       = bit.bor (COMBATLOG_OBJECT_TARGET, COMBATLOG_OBJECT_REACTION_FRIENDLY);
 
     -- http://www.wowwiki.com/API_COMBAT_LOG_EVENT
     function hhtd:COMBAT_LOG_EVENT_UNFILTERED(e, timestamp, event, sourceGUID, sourceName, sourceFlags, destGUID, destName, destFlags, arg9, arg10, arg11, arg12)
 
-	if not sourceGUID then return end
+        if not sourceGUID then return end
 
-	if band (sourceFlags, HOSTILE_OUTSIDER) ~= HOSTILE_OUTSIDER or band(destFlags, PET) == PET then
-	    --self:Print(sourceName, " is kind, its GUID is ", sourceGUID, " the event is ", event);
-	    return;
-	end
+        if band (sourceFlags, HOSTILE_OUTSIDER) ~= HOSTILE_OUTSIDER or band(destFlags, PET) == PET then
+            return;
+        end
 
-	if event:sub(-5) == "_HEAL" and sourceGUID ~= destGUID then
-	    hhtd.EnemyHealers[sourceGUID] = GetTime();
-	end
+        if event:sub(-5) == "_HEAL" and sourceGUID ~= destGUID then
+            hhtd.EnemyHealers[sourceGUID] = GetTime();
+        end
 
     end
 end
--- utils.lua
+++ utils.lua
@@ -8,7 +8,7 @@ Version @project-version@
 This is a very simple and light add-on that rings when you hover or target a
 unit of the opposite faction who healed someone during the last 60 seconds (can
 be configured).
-Now you can spot healers instantly and help them to die.
+Now you can spot those nasty healers instantly and help them to accomplish their destiny!
 
 This add-on uses the Ace3 framework.
 
@@ -35,11 +35,11 @@ HHTD_C.ClassesColors = { };
 
 function hhtd:GetClassColor (EnglishClass)
     if not HHTD_C.ClassesColors[EnglishClass] then
-	if RAID_CLASS_COLORS and RAID_CLASS_COLORS[EnglishClass] then
-	    HHTD_C.ClassesColors[EnglishClass] = { RAID_CLASS_COLORS[EnglishClass].r, RAID_CLASS_COLORS[EnglishClass].g, RAID_CLASS_COLORS[EnglishClass].b };
-	else
-	    HHTD_C.ClassesColors[EnglishClass] = { 0.63, 0.63, 0.63 };
-	end
+        if RAID_CLASS_COLORS and RAID_CLASS_COLORS[EnglishClass] then
+            HHTD_C.ClassesColors[EnglishClass] = { RAID_CLASS_COLORS[EnglishClass].r, RAID_CLASS_COLORS[EnglishClass].g, RAID_CLASS_COLORS[EnglishClass].b };
+        else
+            HHTD_C.ClassesColors[EnglishClass] = { 0.63, 0.63, 0.63 };
+        end
     end
     return unpack(HHTD_C.ClassesColors[EnglishClass]);
 end
@@ -50,9 +50,9 @@ function hhtd:GetClassHexColor(EnglishClass)
 
     if not HHTD_C.HexClassColor[EnglishClass] then
 
-	local r, g, b = self:GetClassColor(EnglishClass);
+        local r, g, b = self:GetClassColor(EnglishClass);
 
-	HHTD_C.HexClassColor[EnglishClass] = ("FF%02x%02x%02x"):format( r * 255, g * 255, b * 255);
+        HHTD_C.HexClassColor[EnglishClass] = ("FF%02x%02x%02x"):format( r * 255, g * 255, b * 255);
 
     end
 
@@ -62,16 +62,16 @@ end
 
 function hhtd:CreateClassColorTables ()
     if RAID_CLASS_COLORS then
-	local class, colors;
-	for class in pairs(RAID_CLASS_COLORS) do
-	    if not class:find(" ") then -- thank to a wonderful add-on that adds the wrong translation "Death Knight" to the global RAID_CLASS_COLORS....
-		hhtd:GetClassHexColor(class);
-	    else
-		RAID_CLASS_COLORS[class] = nil; -- Eat that!
-	    end
-	end
+        local class, colors;
+        for class in pairs(RAID_CLASS_COLORS) do
+            if not class:find(" ") then -- thank to a wonderful add-on that adds the wrong translation "Death Knight" to the global RAID_CLASS_COLORS....
+                hhtd:GetClassHexColor(class);
+            else
+                RAID_CLASS_COLORS[class] = nil; -- Eat that!
+            end
+        end
     else
-	hhtd:Debug("global RAID_CLASS_COLORS does not exist...");
+        hhtd:Debug("global RAID_CLASS_COLORS does not exist...");
     end
 end
 
