1616c59aa4b284f2f4477d9c35772a0ea73c0bdb
Archarodim <archarodim@teaser.fr>
2012-08-30 03:48:56 +0200

    Added support for itIT Localization

--- Localization.lua
+++ Localization.lua
@@ -233,4 +233,10 @@ do
     end
 end
 
+do
+    local L = LibStub("AceLocale-3.0"):NewLocale("HealersHaveToDie", "itIT");
 
+    if L then
+        --@localization(locale="itIT", format="lua_additive_table")@
+    end
+end
