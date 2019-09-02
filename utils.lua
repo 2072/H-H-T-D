56ed5691a4a44cfd8ab657dc39b2c9dde678e0b0
Archarodim <John.Wellesz@gmail.com>
2019-09-03 00:32:27 +0200

    Make it compatible with WoW Classic

--- utils.lua
+++ utils.lua
@@ -86,13 +86,20 @@ function HHTD:GetClassHexColor(englishClass) -- {{{
     return HHTD_C.HexClassColor[englishClass];
 end -- }}}
 
+local NON_CLASSIC_CLASSES = {
+    ["DEMONHUNTER"]    = true,
+    ["DEATHKNIGHT"]    = true,
+    ["MONK"]           = true
+
+};
+
 function HHTD:CreateClassColorTables () -- {{{
     if RAID_CLASS_COLORS then
         local class, colors;
         for class in pairs(RAID_CLASS_COLORS) do
             if LC[class] then -- thank to a wonderful add-on that adds the wrong translation "Death Knight" to the global RAID_CLASS_COLORS....
                 HHTD:GetClassHexColor(class);
-            else
+            elseif not (HHTD_C.WOWC and NON_CLASSIC_CLASSES[class]) then
                 RAID_CLASS_COLORS[class] = nil; -- Eat that!
                 self:Print("|cFFFF0000Stupid value found in _G.RAID_CLASS_COLORS table|r\nThis will cause many issues (tainting), HHTD will display this message until the culprit add-on is fixed or removed, the Stupid value is: '", class, "'");
             end
